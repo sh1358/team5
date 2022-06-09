83646169da4879b8057b0dc5fdd80cac8ab232c9
sooyoung <tndud13580@naver.com>
2022-06-09 20:36:20 +0900

    aaa

---
+++ src/team/teamProject.java
@@ -0,0 +1,20 @@
+package team;
+
+public class teamProject {
+public static void main(String[] args) {
+	
+	int num=0;
+	while(true) {
+		System.out.println("1. 회원관리 2. 이벤트 관리 3. 게시판관리");
+		switch (num) {
+		case 1:break;
+		case 2:break;
+		case 3:break;
+
+		default:
+			break;
+		}
+	}
+	
+}
+}
